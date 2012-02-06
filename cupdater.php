7ae82187406d4061d136dfc69817f96d9a32eb1d
Daniel Dimitrov <daniel@compojoom.com>
2012-02-06 23:15:51 +0100

    initial release

---
+++ cupdater.php
@@ -0,0 +1,249 @@
+<?php
+
+/**
+ * @author Daniel Dimitrov <daniel@compojoom.com>
+ * @copyright    Copyright (C) 2008 - 2012 compojoom.com. All rights reserved.
+ * @license        GNU General Public License version 2 or later
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+/**
+ * Joomla! Update notification plugin
+ * This plugin checks at specific intervals for new updates
+ * and sends a notification if an update is found
+ *
+ * Special thanks to O.Schwab <service@castle4us.de> for coming with the idea
+ * in the first place :)
+ *
+ * @package        Joomla.Plugin
+ * @subpackage    System.cupdater
+ */
+class plgSystemCupdater extends JPlugin
+{
+    private $recipients = array();
+
+    /**
+     * @return bool
+     */
+    public function onAfterRender()
+    {
+        if (!$this->doIHaveToRun()) {
+            return false;
+        }
+
+//      so we are running??? Then let us load some languages
+        $lang = & JFactory::getLanguage();
+        $lang->load('plg_system_cupdater', JPATH_ADMINISTRATOR, 'en-GB', true);
+        $lang->load('plg_system_cupdater', JPATH_ADMINISTRATOR, $lang->getDefault(), true);
+        $lang->load('plg_system_cupdater', JPATH_ADMINISTRATOR, null, true);
+
+
+        //      clear the cache - otherwise we get problems in backend
+        $cache = JFactory::getCache();
+        $cache->clean('com_plugins');
+
+        //      now check for updates
+        $updates = $this->checkForUpdates();
+
+        if (count($updates)) {
+            if (count($updates) == 1) {
+                $title = JText::_('CUPDATER_FOUND_ONE_UPDATE');
+            } else {
+                $title = JText::sprintf('CUPDATER_FOUND_UPDATES', count($updates));
+            }
+
+            $body = '';
+            foreach ($updates as $value) {
+                $body .= JText::_('CUPDATER_UPDATE_FOR') . ': '
+                    . $value->name . ' ' . JText::_('CUPDATER_VERSION') . ': '
+                    . $value->version . ' ' . JText::_('CUPDATER_FOUND') . "\n";
+            }
+            $body .= "\n" . JText::_('CUPDATER_TO_APPLY_UPDATE') . ': '
+                . JURI::base() . ' ' . JText::_('CUPDATER_AND_GO_TO') . "\n";
+
+        } else if ((!count($updates)) && (($this->params->get('mailto_noresult') == 0))) {
+            $title = JText::_('CUPDATER_FOUND_NO_UPDATE_TITLE');
+            $body = JText::_('CUPDATER_FOUND_NO_UPDATE') . ' ' . JURI::base() . "\n";
+        }
+
+        if (count($updates) ||
+            ((!count($updates)) && (($this->params->get('mailto_noresult') == 0)))) {
+
+            $body .= $this->nextUpdateText();
+            $body .= "\n" . JText::_('CUPDATER_DISCLAIMER') . "\n";
+
+            $this->sendMail($title, $body);
+        }
+
+        $this->setLastRunTimestamp();
+
+        return true;
+    }
+
+    /**
+     * @return string - our next update text
+     */
+    private function nextUpdateText()
+    {
+        $body = JText::_("CUPDATER_NEXT_UPDATE_CHECK_WILL_BE") . ' ';
+
+        switch ($this->params->get('notification_period')) {
+            case "24":
+                $body .= JText::_('CUPDATER_TOMORROW') . '.';
+                break;
+            case "168":
+                $body .= JText::_('CUPDATER_IN_ONE_WEEK') . '.';
+                break;
+            case "336":
+                $body .= JText::sprintf('CUPDATER_IN_WEEKS', 2) . '.';
+                break;
+            case "672":
+                $body .= JText::sprintf('CUPDATER_WEEKS', 4) . '.';
+                break;
+        }
+
+        return $body;
+    }
+
+    /**
+     * @param $title - the title of the mail
+     * @param $body - the body of the mail
+     */
+    private function sendMail($title, $body)
+    {
+        $app = JFactory::getApplication();
+        $recipients = $this->getRecipients();
+        $mail = JFactory::getMailer();
+        $mail->addRecipient($recipients);
+        $mail->setSender(array($app->getCfg('mailfrom'), $app->getCfg('fromname')));
+        $mail->setSubject($title);
+        $mail->setBody($body);
+        $mail->Send();
+    }
+
+    /**
+     *
+     * @return array - all recepients
+     */
+    private function getRecipients()
+    {
+        $emails = array();
+
+        if (!count($this->recipients)) {
+            if ($this->params->get('mailto_admins', 0)) {
+                $groups = $this->params->get('mailto_admins');
+                $tmp_emails = $this->getEmailsForUsersInGroups($groups);
+
+                if (!is_array($emails)) {
+                    $emails = array();
+                }
+                $emails = array_merge($tmp_emails, $emails);
+            }
+
+            if ((int)$this->params->get('mailto_custom') == 1 && $this->params->get('custom_email') != "") {
+                $tmp_emails = explode(';', $this->params->get('custom_email'));
+
+                if (!is_array($emails)) {
+                    $emails = array();
+                }
+                $emails = array_merge($tmp_emails, $emails);
+            }
+
+            $tmp = array();
+            foreach ($emails AS $r)
+            {
+                if (in_array($r, $tmp) || trim($r) == "") {
+                    continue;
+                }
+                else {
+                    $this->recipients[] = $r;
+                    $tmp[] = $r;
+                }
+            }
+        }
+
+        return $this->recipients;
+    }
+
+    /**
+     * checks if there are updates available and
+     * @return object - the updates
+     */
+    private function checkForUpdates()
+    {
+        $updater = JUpdater::getInstance();
+        $updater->findUpdates(0, 0);
+
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select('*');
+        $query->from('#__updates')->where('extension_id != 0');
+        $db->setQuery($query);
+
+        return $db->loadObjectList();
+    }
+
+    /**
+     * "Do I have to run?" - the age old question. Let it be answered by checking the
+     * last execution timestamp, stored in the component's configuration.
+     *
+     * this function is copied from the asexpirationnotify plugin so all credits go to
+     * Nicholas K. Dionysopoulos / AkeebaBackup.com
+     * @return bool
+     */
+    private function doIHaveToRun()
+    {
+        $params = $this->params;
+        $lastRunUnix = $params->get('plg_cupdate_timestamp', 0);
+        $dateInfo = getdate($lastRunUnix);
+        $nextRunUnix = mktime(0, 0, 0, $dateInfo['mon'], $dateInfo['mday'], $dateInfo['year']);
+        $nextRunUnix += $params->get('notification_period', 24) * 3600;
+        $now = time();
+        return ($now >= $nextRunUnix);
+    }
+
+    /**
+     * Saves the timestamp of this plugin's last run
+     * this function is copied from the asexpirationnotify plugin so all credits go to
+     * Nicholas K. Dionysopoulos / AkeebaBackup.com
+     *
+     */
+    private function setLastRunTimestamp()
+    {
+        $lastRun = time();
+        $params = $this->params;
+        $params->set('plg_cupdate_timestamp', $lastRun);
+
+        $db =& JFactory::getDBO();
+
+        $data = $params->toString('JSON');
+        $query = $db->getQuery(true);
+        $query->update('#__extensions');
+        $query->set('params = ' . $db->Quote($data));
+        $query->where('element = "cupdater" AND type = "plugin"');
+        $db->setQuery($query);
+        $db->query();
+    }
+
+    /**
+     *
+     * @param array $groups - the user group
+     * @return mixed
+     */
+    private function getEmailsForUsersInGroups(array $groups)
+    {
+
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select('a.email');
+        $query->from('#__user_usergroup_map AS map');
+        $query->leftJoin('#__users AS a ON a.id = map.user_id');
+        $query->where('map.group_id IN (' . implode(',', $groups) . ')');
+
+        $db->setQuery($query);
+        return $db->loadColumn();
+    }
+
+}
